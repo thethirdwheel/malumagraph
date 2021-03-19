0a4fd4c3eab40106dc6a5459d7419fb109fbb9e2
Rory Carmichael <carmichael.rory@gmail.com>
2021-03-18 23:04:00 -0700

    Add functions for producing sparklines, contour maps, and heat maps

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from plotly.subplots import make_subplots
 import numpy as np
 import plotly.figure_factory as ff
 
-#mapping from arpabet to roundness
+#mapping from arpabet to roundness (should move to a file)
 phone2roundness = {
 	'AA' : 0.70, #Non-Front
 	'AE' : 0.49, #Front
@@ -64,6 +64,37 @@ phone2roundness = {
 }
 word2phone = cmudict.dict()
 
+def contour_map(scores):
+	temp_scores = scores
+	temp_scores[temp_scores == None] = 0
+	temp_scores = np.vstack([temp_scores, np.zeros_like(temp_scores[0])])
+	temp_scores = np.insert(temp_scores, 0, 0, axis=0)
+	temp_scores = np.insert(temp_scores, 0, 0, axis=1)
+	fig = go.Figure(data = go.Contour(z=temp_scores, line_smoothing=0.85))
+	return fig
+
+def annotated_heat_map(scores, sounds):
+	#Have to flip the arrays so that text is displayed in order
+	temp_scores = np.flip(scores,0)
+	temp_scores[temp_scores == None] = 0
+	fig = ff.create_annotated_heatmap(temp_scores, annotation_text=np.flip(sounds,0))
+	return fig
+
+def sparklines(scores, cols):
+	num_lines = len(scores)
+	fig = make_subplots(num_lines, 1)
+	for index, line in enumerate(scores, start=1):
+		fig.add_trace(go.Scatter(y=line, x=list(range(0,cols)), mode='lines'), index, 1)
+	fig.update_xaxes(visible=False, fixedrange=True)
+	fig.update_yaxes(visible=False, fixedrange=True)
+	fig.update_layout(annotations=[], overwrite=True)
+	fig.update_layout(
+		showlegend=False,
+		plot_bgcolor="white",
+		margin=dict(t=10,l=10,b=10,r=10)
+		)
+	return fig
+
 punctuation_table = str.maketrans(dict.fromkeys(string.punctuation))
 poem_scores = []
 poem_stresses = []
@@ -83,14 +114,13 @@ with open("corpus.txt") as f:
 					if phone[-1].isdigit():
 						stresses.append(phone[-1])
 						phone = phone[0:-1]
-						#syllableCounter += 1
 					else:
 						stresses.append(0)
 					scores.append(phone2roundness[phone])
 					sounds.append(phone)
-				scores.append(0.5)
-				stresses.append(0)
-				sounds.append("")
+				scores.append(None)
+				stresses.append(None)
+				sounds.append(None)
 		poem_scores.append(scores)
 		poem_stresses.append(stresses)
 		poem_sounds.append(sounds)
@@ -99,21 +129,24 @@ for i in poem_scores:
 		cols = len(i)
 print(f"rows: {rows} cols: {cols}", file=sys.stderr)
 
-justified_scores = np.zeros(shape=(rows,cols))
+justified_scores = np.full(shape=(rows,cols), fill_value=None)
 justified_sounds= np.empty(shape=(rows,cols), dtype="str")
 
 for i, row in enumerate(poem_scores):
 	for j, val in enumerate(row):
-		justified_scores[i][j] = val
+		#Adjust scores to be negative if spiky, positive if round
+		if val:
+			justified_scores[i][j] = val - 0.5
+		else:
+			justified_scores[i][j] = None
 		justified_sounds[i][j] = poem_sounds[i][j]
 print(justified_scores, file=sys.stderr)
 print(justified_sounds, file=sys.stderr)
 
-#fig = make_subplots(rows=1, cols=2)
-#fig.add_trace(go.Contour(z=poem_scores, line_smoothing=0.85), 1, 2)
-fig = ff.create_annotated_heatmap(np.flip(justified_scores,0), annotation_text=np.flip(justified_sounds,0))
+fig = sparklines(justified_scores, cols)
+#fig = annotated_heat_map(justified_scores, justified_sounds)
+#fig = contour_map(justified_scores)
 img = fig.to_image(format="svg")
 with os.fdopen(sys.stdout.fileno(), "wb", closefd=False) as stdout:
     stdout.write(img)
     stdout.flush()
-
